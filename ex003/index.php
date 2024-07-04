2d4ece67e42ffadc61ee09c8951369a8d496c6d1
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2024-07-04 16:47:03 -0300

    aula 2 e 3

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = "Israel";
+    $sobrenome = "Moreira";
+    $idade = 27;
+    $peso = 73.5;
+    $casado = true;
+
+    const PAIS = "Brasil";
+
+    echo "Olá, me chamo $nome $sobrenome, tenho $idade de idade e peso $peso quilos e sou $casado " . PAIS;
+    ?>
+    
+</body>
+</html>
